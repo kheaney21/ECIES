96c0e4fa300ae970efe26b36471b4357976bd9ec
Kelly Heaney <31257783+kheaney21@users.noreply.github.com>
2018-11-07 07:29:20 -0800

    Update ECIES.py

--- ECIES.py
+++ ECIES.py
@@ -1,122 +1,153 @@
 import random
+import sys
+from Crypto.Hash import SHA256
+from Crypto.Cipher import AES
+from Crypto import Random
 
-#input since p & g are shared and not secret
-def genPrivKey(p, g):
-	'Generate a private key from 2 prime numbers'
+
+class ECIES:
+	'Class for ECIES object'
+	def __init__(self, p, g):
+		self.privKeyResult = self.genPrivKey(p, g)
+		self.privKey = self.privKeyResult[0]
+		self.result = self.privKeyResult[1]
 	
-	#limit for number range
-	n = 10000
+	#def EC():
+		# curve y^2 = x^3 + ax + b
+
+	#input since p & g are shared and not secret
+	def genPrivKey(self, p, g):
+		'Generate a private key from 2 prime numbers'
 	
-	#generate random number a for private key
-	a = random.randint(2, n)
+		#limit for number range
+		n = 10000
 	
-	#calculate result A
-	A = g**a % p
-	#print("a " + str(a))
-	#print("g " + str(g))
-	#print("p " + str(p))
-	#print("A " + str(A))
+		#generate random number a for private key
+		a = random.randint(2, n)
 	
+		#calculate result A
+		A = g**a % p
+
+		return (a, A)
 	
-	return (a, A)
+	def genPubKey(self, a, B, p):
+		'Generate public key with secret number a, result B, and shared p'
 	
-def genPubKey(a, B, p):
-	'Generate public key with secret number a, result B, and shared p'
+		#calculate public key
+		pubKey = B**a % p
 	
-	#calculate public key
-	pubKey = B**a % p
-	#print("B " + str(B))
-	#print("a " + str(a))
-	#print("p " + str(p))
+		return pubKey
 	
-	return pubKey
-	
-def genPrime(n, k):
-	'Generate a prime number'
-	y = random.randrange(80, 120)
-	x = random.randrange(3, n, 2)
-	count = 0
-	print("k " + str(k))
-	while (not isPrime(x, k) and count < k):
+	def genPrime(n, k):
+		'Generate a prime number'
+
 		x = random.randrange(51, n, 2)
-		count = count + 1
-		print(str(count) + " x " + str(x))
-	#print("final x1 " + str(x1))
-	print("final x " + str(x))
-	print("count " + str(count))
-	return x
+		count = 0
+		print("k " + str(k))
+		while (not isPrime(x, k) and count < k):
+			x = random.randrange(51, n, 2)
+			count = count + 1
+			#print(str(count) + " x " + str(x))
+		print("final x " + str(x))
+		print("count " + str(count))
+		return x
 			
 #check if prime via miller-rabin test
-def isPrime(n, k):
-	'Check if prime via Miller-Rabin test'
+	def isPrime(n, k):
+		'Check if prime via Miller-Rabin test'
 		
-	#calculate d
-	temp = n
-	s = 0
-	while temp % 2 == 0:
-		temp = temp/2
-		s = s + 1
+		#calculate d
+		temp = n
+		s = 0
+		while temp % 2 == 0:
+			temp = temp/2
+			s = s + 1
 			
-	d = n/(2**s)
+		d = n/(2**s)
 		
-	count = 0
-	while count < k:
-		#update counter
-		count = count + 1
-		#generate pseudorandom number
-		a = random.randint(2, n - 1)
-		x = a**d % n
-		if (x == 1 or x == n - 1):
-			continue
-		for i in range (s-1):
-			x = (x**2) % n
-			if x == 1:
-				return False
-			if x == n - 1:
+		count = 0
+		while count < k:
+			#update counter
+			count = count + 1
+			#generate pseudorandom number
+			a = random.randint(2, n - 1)
+			x = a**d % n
+			if (x == 1 or x == n - 1):
 				continue
+			for i in range (s-1):
+				x = (x**2) % n
+				if x == 1:
+					return False
+				if x == n - 1:
+					continue
 				
-		return False
-	return True
+			return False
+		return True
 	
-def verifyKeys(a, b):
-	if a == b:
-		print("Keys match")
-		print(a)
-	else:
-		print("Keys do not match")
-		print("a " + str(a))
-		print("b " + str(b))
+	def verifyKeys(self, a, b):
+		'Verify that they keys a & b match'
+		if a == b:
+			print("Keys match")
+			print(a)
+		else:
+			print("Keys do not match")
+			print("a " + str(a))
+			print("b " + str(b))
+		
+	def getHash(x):
+		'Get SHA 256 Hash'
+		x = SHA256.new(x)
+		return x
+	
+	def encrypt(key, filename):
+		'Encrypts a file based on ECIES and Diffie Hellman'
+		#read file as string
+		plainText = open(filename, "r+")
+		iv = Random.new().read(AES.block_size)
+		cipher = AES.new(getHash(key), AES.MODE_CBC, iv)
+		plainText.write(cipher)
 
+	def decrypt(key, filename):
+		'Decrypts a file based on ECIES and Diffie Hellman'
+		cipherText = open(filename, "r+")
+		iv = cipherText[:AES.block_size]
+		cipher = AES.new(getHash(key), AES.MODE_CBC, iv)
+		plainText = cipher.decrypt(cipherText[AES.block_size:])
+		filename.write(plainText)
+	
+
+#---------------------------------------------
 
 #max times to check if prime
 k = 10000
 #limit for number range
 n = 10000
 #generate random prime numbers g and p
-p = genPrime(n, k)
-g = genPrime(n, k)
+#p = genPrime(n, k)
+#g = genPrime(n, k)
 
-#generate private key for A
+	#generate private key for A
 
-privKeyA = genPrivKey(p, g)
-A = privKeyA[1]
-print("A " + str(A))
+#privKeyA = genPrivKey(p, g)
+#A = privKeyA[1]
+#print("A " + str(A))
 
-#generate private key for B
+	#generate private key for B
 
-privKeyB = genPrivKey(p, g)
-B = privKeyB[1]
-print("B " + str(B))
+#privKeyB = genPrivKey(p, g)
+#B = privKeyB[1]
+#print("B " + str(B))
 
-#generate public key for A
-a = privKeyA[0]
-pubKeyA = genPubKey(a, B, p)
+	#generate public key for A
+#a = privKeyA[0]
+#pubKeyA = genPubKey(a, B, p)
 #print("pubKey " + str(pubKeyA))
 
+	#generate public key for B
+#b = privKeyB[0]
+#pubKeyB = genPubKey(b, A, p)
 
-#generate public key for B
-b = privKeyB[0]
-pubKeyB = genPubKey(b, A, p)
+	#verify that the keys match
+#verifyKeys(pubKeyA, pubKeyB)
 
-#verify that the keys match
-verifyKeys(pubKeyA, pubKeyB)
+
