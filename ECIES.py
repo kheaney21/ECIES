aeb116b81adc6cedf13824a7f64e8245afde708f
Kelly Heaney <31257783+kheaney21@users.noreply.github.com>
2018-11-07 04:01:04 -0800

    Key generation
    Key generation seems to be working

--- ECIES.py
+++ ECIES.py
@@ -1,33 +1,49 @@
 import random
 
 #input since p & g are shared and not secret
-def genPrivkey(self, p, g):
+def genPrivKey(p, g):
 	'Generate a private key from 2 prime numbers'
 	
 	#limit for number range
-	n = 10,000
+	n = 10000
 	
 	#generate random number a for private key
 	a = random.randint(2, n)
 	
 	#calculate result A
 	A = g**a % p
+	#print("a " + str(a))
+	#print("g " + str(g))
+	#print("p " + str(p))
+	#print("A " + str(A))
+	
 	
 	return (a, A)
 	
-def genPubKey(self, a, B, p):
+def genPubKey(a, B, p):
 	'Generate public key with secret number a, result B, and shared p'
 	
 	#calculate public key
 	pubKey = B**a % p
+	#print("B " + str(B))
+	#print("a " + str(a))
+	#print("p " + str(p))
 	
 	return pubKey
 	
 def genPrime(n, k):
 	'Generate a prime number'
-	x = random.randrange(3, 10000, 2) #had to hardcode, n is not working
-	while not (isPrime(x, k)):
-		x = random.randrange(3, 10000, 2) #had to hardcode, n is not working
+	y = random.randrange(80, 120)
+	x = random.randrange(3, n, 2)
+	count = 0
+	print("k " + str(k))
+	while (not isPrime(x, k) and count < k):
+		x = random.randrange(51, n, 2)
+		count = count + 1
+		print(str(count) + " x " + str(x))
+	#print("final x1 " + str(x1))
+	print("final x " + str(x))
+	print("count " + str(count))
 	return x
 			
 #check if prime via miller-rabin test
@@ -62,15 +78,18 @@ def isPrime(n, k):
 		return False
 	return True
 	
-def verifyKeys(self, a, b):
+def verifyKeys(a, b):
 	if a == b:
 		print("Keys match")
+		print(a)
 	else:
 		print("Keys do not match")
+		print("a " + str(a))
+		print("b " + str(b))
 
 
 #max times to check if prime
-k = 100
+k = 10000
 #limit for number range
 n = 10000
 #generate random prime numbers g and p
@@ -81,18 +100,23 @@ g = genPrime(n, k)
 
 privKeyA = genPrivKey(p, g)
 A = privKeyA[1]
+print("A " + str(A))
 
 #generate private key for B
 
 privKeyB = genPrivKey(p, g)
-B = privKeyA[1]
+B = privKeyB[1]
+print("B " + str(B))
 
 #generate public key for A
+a = privKeyA[0]
 pubKeyA = genPubKey(a, B, p)
+#print("pubKey " + str(pubKeyA))
+
 
 #generate public key for B
+b = privKeyB[0]
 pubKeyB = genPubKey(b, A, p)
 
 #verify that the keys match
 verifyKeys(pubKeyA, pubKeyB)
-	
